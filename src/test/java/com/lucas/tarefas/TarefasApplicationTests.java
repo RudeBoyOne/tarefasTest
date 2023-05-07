f2b37b49dcb4cf65a58aa8c34b4681d7fb4ed783
Lucas Ferreira Nogueira <lucas.ferreiranogueira@outlook.com>
2023-05-07 16:59:29 -0300

    FIX: ajustes nos testes e adequação para build e deploy

--- src/test/java/com/lucas/tarefas/TarefasApplicationTests.java
+++ src/test/java/com/lucas/tarefas/TarefasApplicationTests.java
@@ -2,8 +2,10 @@ package com.lucas.tarefas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TarefasApplicationTests {
 
 	@Test
